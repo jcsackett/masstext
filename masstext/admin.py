01b113936babb2eee45bc1771e7cd71535247d05
Jon Sackett <jc@alice.local>
2010-03-16 22:16:22 -0400

    * Added Calllist/group functionality.

--- masstext/admin.py
+++ masstext/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.admin import site
 
-from models import PhoneNumber
+from models import PhoneNumber, CallList
 
 site.register(PhoneNumber)
+site.register(CallList)
--- masstext/models.py
+++ masstext/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+
 
 class PhoneNumberManager(models.Manager):
     def get_reply_numbers(self, number):
@@ -13,13 +14,22 @@ class PhoneNumberManager(models.Manager):
             return [] 
         return [pn.number for pn in self.exclude(number=number)]
 
+
 class PhoneNumber(models.Model):
     """Phone numbers for the mass texting."""
     number = models.CharField(max_length=10)
     owner = models.ForeignKey(User)
-
+    call_list = models.ForeignKey('CallList')
+    can_send = models.BooleanField(default=True)
+    
     def __unicode__(self):
         return u'%s, for %s' % (self.number, self.owner.username)
 
     objects = PhoneNumberManager()
 
+
+class CallList(models.Model):
+    """Implements a basic call list for the reflection, with minimal permissions."""
+    name = models.CharField(max_length=255)
+    moderated = models.BooleanField(default=False)
+    
