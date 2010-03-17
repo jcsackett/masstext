467b824fc2e7d617ec1bf438ff747074b5abeda9
Jon Sackett <jc@alice.local>
2010-03-16 22:15:01 -0400

    * Fixed up tests to be a little cleaner and support group unittests.

--- masstext/tests/tests.py
+++ masstext/tests/tests.py
@@ -5,17 +5,8 @@ from django.core.urlresolvers import reverse
 from masstext.models import PhoneNumber
 
 class TextingTestCase(TestCase):
-    def setUp(self):
-        u1 = User(username='user1')
-        u2 = User(username='user2')
-        u1.save()
-        u2.save()
-
-        pn1 = PhoneNumber(owner=u1, number='9195555555')
-        pn2 = PhoneNumber(owner=u2, number='9195555556')
-        pn1.save()
-        pn2.save()
-
+    fixtures = ['phonenumber.json']
+    
     def test_mass_text_get(self):
         #tests that gets return nothing
         url = reverse('masstext')
@@ -31,30 +22,29 @@ class TextingTestCase(TestCase):
             From='9195555555',
             Body='This is a message.'
         )
-        expected = ''.join('<?xmlversion="1.0"encoding="UTF-8"?><Response><Sms to="9195555556">user1 says: This is a message.</Sms></Response>'.split())
+        expected = '''
+        <?xmlversion="1.0"encoding="UTF-8"?>
+        <Response>
+            <Sms to="9195555556">test_one says: This is a message.</Sms>
+            <Sms to="9195555557">test_one says: This is a message.</Sms>
+        </Response>
+        '''
+        expected = ''.join(expected.split())
         r = c.post(url, data)
         received = ''.join(r.content.split())
         self.assertEqual(received, expected)
 
+
 class PhoneNumberManagerTestCase(TestCase):
-    def setUp(self):
-        u1 = User(username='user1')
-        u2 = User(username='user2')
-        u1.save()
-        u2.save()
-
-        pn1 = PhoneNumber(owner=u1, number='9195555555')
-        pn2 = PhoneNumber(owner=u2, number='9195555556')
-        pn1.save()
-        pn2.save()
-
+    fixtures = ['phonenumber.json']
+    
     def test_get_reply_numbers_with_valid_number(self):
         #tests that a valid reply set is returned for a valid number
         numbers = PhoneNumber.objects.all().values()
         sender = numbers[0]['number']
-        receiver = numbers[1]['number']
+        receiver = [n['number'] for n in numbers[1:]]
         reply_set = PhoneNumber.objects.get_reply_numbers(sender)
-        self.assertEqual([receiver], reply_set)
+        self.assertEqual(receiver, reply_set)
 
     def test_get_reply_numbers_with_invalid_number(self):
         #tests that an invalid number gets no reply set
